fb16d8cddb27cc588c2f590434e7a9a48fb54c66
Daniel <dchoiboi00@gmail.com>
2022-03-05 18:06:12 +0900

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, nam eos
+        voluptatem voluptatibus quibusdam molestiae in eligendi optio nemo totam
+        ea. Voluptates quibusdam consectetur corporis omnis necessitatibus
+        sapiente, beatae hic. Necessitatibus molestiae rem possimus sed, tempore
+        natus temporibus quaerat deleniti!
+      </p>
+    </section>
+  );
+};
+
+export default About;
